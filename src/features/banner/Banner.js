7959fc95176cf2fec270ba5436515b46f31d9bc4
ndorrh <ndorrh@gmail.com>
2022-11-13 19:13:28 +0100

    Add banner component and Style banner component also added state to the show state button

---
+++ src/features/banner/Banner.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-expressions */
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import './Banner.css';
+
+export default function Banner() {
+  const [state, setState] = useState(
+    {
+      hide: 'Show Dates',
+      showSearchDatePicker: false,
+    },
+  );
+
+  const handleDate = () => {
+    (state.hide === 'Show Dates') ? setState({
+      hide: 'Hide',
+      showSearchDatePicker: true,
+    }) : setState({
+      hide: 'Show Dates',
+      showSearchDatePicker: false,
+    });
+  };
+
+  return (
+    <div className="banner">
+      {state.showSearchDatePicker && <h1>Date picker</h1>}
+      <div className="banner__search">
+        <Button
+          variant="outlined"
+          className="banner__searchBtn"
+          onClick={handleDate}
+        >
+          {state.hide}
+        </Button>
+      </div>
+      <div className="banner__info">
+        <h1>Get out and stretch your imagination</h1>
+        <h5>Plan a different type of Getaway to uncover the hidden gems near you</h5>
+        <Button variant="outlined">Explore Nearby</Button>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../features/banner/Banner';
 import './Home.css';
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home component</h1>
+      <Banner />
     </div>
   );
 }

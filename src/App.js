e18105cfb57f3c091017caf4be6d76431c7a9066
ndorrh <ndorrh@gmail.com>
2022-11-13 10:33:55 +0100

    Create Template for Home page component and Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from './common/Header';
+import Home from './pages/Home';
 
 const App = () => (
   // BEM
   <div className="app">
+    <Header />
+    <Home />
     <h1>Let&apos;s Build Airbnb Clone</h1>
+    {/* Home */}
+    {/* Header */}
+    {/* Banner */}
+    {/* Cards */}
+    {/* Footer */}
   </div>
 );
 
---
+++ src/common/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>Header component</h1>
+    </div>
+  );
+}
